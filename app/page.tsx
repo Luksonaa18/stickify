11375ea65bab7042ba209c32edbf216061f361c1
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2025-07-04 01:07:44 +0400

    fix:use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Header from "@/components/header/Header";
 import StickyNotes from "@/components/stickynotes/StickyNotes";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
